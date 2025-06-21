880f23a01f0e897b6e134ff432139f13ebf0dcfb
unviersal-2 <connorpolson0324@gmail.com>
2025-06-21 16:57:16 -0400

    Update ImbuedSpellPacket.java

--- src/main/java/com/github/jarva/arsadditions/common/item/ImbuedSpellPacket.java
+++ src/main/java/com/github/jarva/arsadditions/common/item/ImbuedSpellPacket.java
@@ -1,1 +1,79 @@
+package com.github.jarva.arsadditions.common.item;
 
+import com.github.jarva.arsadditions.setup.registry.AddonItemRegistry;
+import com.hollingsworth.arsnouveau.api.event.SpellCostCalcEvent;
+import com.hollingsworth.arsnouveau.api.spell.AbstractCaster;
+import com.hollingsworth.arsnouveau.api.spell.SpellCaster;
+import com.hollingsworth.arsnouveau.common.items.SpellParchment;
+import com.hollingsworth.arsnouveau.setup.registry.DataComponentRegistry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME)
+public class ImbuedSpellParchment extends SpellParchment {
+    public ImbuedSpellParchment() {
+        super(AddonItemRegistry.defaultItemProperties().component(DataComponentRegistry.SPELL_CASTER, new SpellCaster()));
+    }
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+    
+        // Add custom NBT
+        CompoundTag tag = stack.getOrCreateTag();
+        tag.putFloat("Level", 1.0);
+
+        return InteractionResultHolder.success(stack);
+    }
+    
+    @Override
+    public int getUseDuration(ItemStack stack, LivingEntity entity) {
+        int cost = getSpellCaster(stack).getSpell().getCost();
+        int seconds = -Math.floorDiv(-cost, 1);
+        float value = tag.getString("Level");
+        return Math.floorDiv(seconds,value);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        player.startUsingItem(usedHand);
+        ItemStack stack = player.getItemInHand(usedHand);
+        return InteractionResultHolder.consume(stack);
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack stack) {
+        return UseAnim.BOW;
+    }
+
+    @Override
+    public void onUseTick(Level level, LivingEntity livingEntity, ItemStack stack, int remainingUseDuration) {
+        if (!(livingEntity.getCooldowns().isOnCooldown(stack.getItem()))) return;
+
+        AbstractCaster<?> caster = getSpellCaster(stack);
+        InteractionResultHolder<ItemStack> result = caster.castSpell(level, livingEntity, InteractionHand.MAIN_HAND, Component.translatable("ars_nouveau.invalid_spell"));
+        int cost = getSpellCaster(stack).getSpell().getCost();
+        int seconds = -Math.floorDiv(-cost, 0.75);
+        float value = tag.getString("Level");
+        if (result.getResult().consumesAction()) {
+            stack.consume(1, livingEntity);
+        }
+        
+        livingEntity.stopUsingItem();
+        player.getCooldowns().addCooldown(stack, Math.floorDiv(seconds,value));
+    }
+
+    @SubscribeEvent
+    public static void calcSpellCost(SpellCostCalcEvent event) {
+        if (event.context.getCasterTool().getItem() instanceof ImbuedSpellParchment) {
+            event.currentCost = 0;
+        }
+    }
+}
